30e3fedec01379b2309010dc501c21a2f6b03b11
SergZels <92048923+SergZels@users.noreply.github.com>
2024-10-16 08:29:54 +0300

    first

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
+
+func main() {
+	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
+	// to see how GoLand suggests fixing it.
+	s := "Sergii"
+	tim := time.Now().Format("2006-01-02")
+	fmt.Printf("Hello and welcome, %s %s!", s, tim)
+
+}
