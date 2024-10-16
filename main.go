cfeb9da7288d25494ebadf4022d220d067bd9f37
SergZels <92048923+SergZels@users.noreply.github.com>
2024-10-16 09:07:28 +0300

    lesson 4 done

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
 
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "Sergii"
-	tim := time.Now().Format("2006-01-02")
-	fmt.Printf("Hello and welcome, %s %s!", s, tim)
 
+	//---------------lesson1------------------------
+	//	s := "Sergii"
+	//	tim := time.Now().Format("2006-01-02")
+	//	fmt.Printf("Hello and welcome, %s %s!", s, tim)
+
+	//--------------lesson 4-------------------------------------
+	summ := 0
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+		summ += i
+	}
+	fmt.Printf("summ:%d\n", summ)
 }
